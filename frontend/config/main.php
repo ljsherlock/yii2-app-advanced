81092ee469a208584c968fd13711236a8c8a39b6
ljsherlock <lewis@ljsherlock.com>
2018-05-06 23:57:44 +0700

    feat: add requirejs view class

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,6 +36,26 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'view' => [
+            'class'=> 'h8every1\requirejsview\View',
+            'mainJsUrl'=> '@web/dist/js/main.js',
+            // 'class' => 'yii\web\View',
+            'renderers' => [
+                'twig' => [
+                    'class' => 'yii\twig\ViewRenderer',
+                    'cachePath' => '@runtime/Twig/cache',
+                    // Array of twig options:
+                    'options' => [
+                        'auto_reload' => true,
+                    ],
+                    'globals' => [
+                        'html' => ['class' => '\yii\helpers\Html'],
+                    ],
+                    'uses' => ['yii\bootstrap'],
+                ],
+                // ...
+            ],
+        ],
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
